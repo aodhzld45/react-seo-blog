022a4ae255a9a9f4699bf17bc02686b2fd927ce6
hyunseok <prking94@naver.com>
2023-11-17 18:31:48 +0900

    2023-11-17 App.js 글변경 테스트

--- src/App.js
+++ src/App.js
@@ -6,9 +6,17 @@ function App() {
   let logo = 'SEO BLOG'
   const [postTitle, setPostTitle] = useState(['남자 옷 추천', '여자 옷 추천', '아동 옷 추천']);
   const [goodIcons, setGoodIcons] = useState(0);
+  const [post, setPost] = useState(['여자 코트 추천','뭘 꼬라바', '아동 코트 추천']);
 
   const handleGoodClick = (e) => {
     setGoodIcons(goodIcons => goodIcons + 1);
+  }
+
+  const handleChangePost = () => {
+    // 두 번째 글(post[1])로 변경
+    setPost(post => [post[1], post[1], post[1]]);
+    // 좋아요 수 초기화
+    setGoodIcons(0);
   }
 
 
@@ -18,15 +26,19 @@ function App() {
         <h4 style={{ fontSize : '16px' }}>{ logo } </h4>
       </div>
 
+      <button onClick={handleChangePost}>
+        글 변경
+      </button>
+
       <div className='list'>
         <h4>{postTitle[0]}<span onClick={handleGoodClick}>👍</span> {goodIcons} </h4>
-        <p>남자 코트</p>
+        <p>{post[0]}</p>
 
         <h4>{postTitle[1]}</h4>
-        <p>여자 코트</p>
+        <p>{post[1]}</p>
         
         <h4>{postTitle[2]}</h4>
-        <p>아동 코트</p>
+        <p>{post[2]}</p>
 
 
       </div>
